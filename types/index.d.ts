51dca76bb3a566dfc8ec259ff69ed437f95ce586
Stephen Muñez <sjtmunez@gmail.com>
2025-08-24 14:12:24 +0800

    feat: appointment page

--- types/index.d.ts
+++ types/index.d.ts
@@ -38,3 +38,21 @@ declare interface RegisterUserParams extends CreateUserParams {
   treatmentConsent: boolean;
   disclosureConsent: boolean;
 }
+
+declare type CreateAppointmentParams = {
+  userId: string;
+  patient: string;
+  primaryPhysician: string;
+  reason: string;
+  schedule: Date;
+  status: Status;
+  note: string | undefined;
+};
+
+declare type UpdateAppointmentParams = {
+  appointmentId: string;
+  userId: string;
+  timeZone: string;
+  appointment: Appointment;
+  type: string;
+};
